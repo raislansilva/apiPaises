a381e0b77e74e22f49701ff94c16a979a873ea48
raislan <raislan@live.com>
2018-07-31 22:47:03 -0300

    infinit controle

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PaisesProvider } from '../../providers/paises/paises';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -8,17 +9,38 @@ import { PaisesProvider } from '../../providers/paises/paises';
 })
 export class HomePage {
   paises: {};
+  page = 0;
+  pageMax = 3;
+
   constructor(public navCtrl: NavController,
+    private httpClient: HttpClient,
     private paisesProvider: PaisesProvider
-  ) {}
+  ) {
+    this.listaPaises();
+   }
 
   ionViewDidLoad() {
     this.listaPaises();
   }
 
- listaPaises(){
+  listaPaises(infiniteScroll?) {
     this.paisesProvider.listaPaises()
-    .subscribe(paises => this.paises = paises);
+      .subscribe(paises => this.paises = paises);
+
+      if (infiniteScroll) {
+        infiniteScroll.complete();
+      }
   }
+
+  loadMore(infiniteScroll) {
+    this.page++;
+    this.listaPaises(infiniteScroll);
+ 
+    if (this.page === this.pageMax) {
+      infiniteScroll.enable(false);
+    }
+  }
+
+  
 
 }
